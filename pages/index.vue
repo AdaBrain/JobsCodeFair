22fe43a68736f2fc00e33399347d6d833b93e674
sirimainson <sirimainson@gmail.com>
2019-08-02 02:16:29 +0700

    add image for mockup

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,73 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        JobsCodeFair
-      </h1>
-      <h2 class="subtitle">
-        My unreal Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <div id="header" :class="{ smaller: isActive }">
+      <div class="module" id="title">
+        <h1>
+          <a href="/">2019 JobsCodeFair</a>
+        </h1>
+      </div>
+      <div class="module" id="date">
+        <h1>
+          <a href="/">Aug. 31</a>
+        </h1>
+      </div>
+      <div class="module" id="date">
+        <h1>
+          <a href="/">Register</a>
+        </h1>
       </div>
     </div>
+    <div id="container">
+      <div id="neonsigns">
+        <div class="module">
+          <img src="~/assets/computer.jpg" alt class="image-background" />
+        </div>
+      </div>
+      <div id="speakers">
+        <div id="home_intro"></div>
+        <div class="module">
+          <img src="~/assets/man.jpg" alt class="image-background" />
+        </div>
+        <div class="module">
+          <img src="~/assets/man.jpg" alt class="image-background" />
+        </div>
+        <div class="module">
+          <img src="~/assets/man.jpg" alt class="image-background" />
+        </div>
+        <div class="module">
+          <img src="~/assets/man.jpg" alt class="image-background" />
+        </div>
+      </div>
+      <div id="sponsors_home">
+        <div id="presenting">
+          <h1>Presenting <span><br /></span>Sponsors</h1>
+        </div>
+      </div>
+    </div>
+    <div class="footer"></div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import '~/assets/css/style.css'
 
 export default {
-  components: {
-    Logo
+  data () {
+    return {
+      windowTop: 0,
+      isActive: false
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', () => {
+      this.windowTop = window.scrollY
+      if (window.scrollY > 5) {
+        this.isActive = true
+      } else {
+        this.isActive = false
+      }
+      console.log(this.windowTop)
+    })
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
