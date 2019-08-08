a36989756ccd6fb6aea521a3976cceadafc231e8
sirimainson <sirimainson@gmail.com>
2019-08-09 06:41:00 +0700

    add job title speaker

--- pages/index.vue
+++ pages/index.vue
@@ -21,21 +21,26 @@
         <img src="~/assets/images/p_pup.png" alt class="image-background" />
         <h1>P'Pup</h1>
         <h2>Apaichon Punopas</h2>
+        <h3>CTO @ 2bsimple</h3>
       </div>
       <div class="module">
         <img src="~/assets/images/p_tik.png" alt class="image-background" />
         <h1>P'Tick</h1>
         <h2>Jiraporn Changsamlee</h2>
+        <h3>Service Delivery Manager</h3>
       </div>
       <div class="module">
         <img src="~/assets/images/p_pp.png" alt class="image-background" />
         <h1>P'PP</h1>
         <h2>Pornpat Paethong</h2>
+        <h3>Co-founder</h3>
+        <h3>Software Engineer & Technical Lead</h3>
       </div>
       <div class="module">
         <img src="~/assets/images/p_ada.png" alt class="image-background" />
         <h1>P'Ada</h1>
         <h2>Akhane Kaminkure</h2>
+        <h3></h3>
       </div>
     </div>
     <div id="sponsors_home">
