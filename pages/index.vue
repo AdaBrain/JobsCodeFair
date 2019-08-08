7b1a3339626150745ce69a7070f3d9375e027d36
sirimainson <sirimainson@gmail.com>
2019-08-09 06:30:36 +0700

    create firebase

--- pages/index.vue
+++ pages/index.vue
@@ -5,22 +5,37 @@
         <img src="~/assets/images/jcf.png" alt class="image-background" />
       </div>
     </div>
+
     <div id="speakers">
+      <div id="home_intro">
+        <h1>Speakers</h1>
+        <!-- <h3>10-year Anniversary</h3> -->
+        <!-- <h1>
+          <a
+            href="https://www.underconsideration.com/brandnewconference/register/conference-2019/"
+          >Register</a>
+        </h1> -->
+        <!-- <h2>ONLINE REGISTRATION CLOSES OCTOBER 10, 2019</h2> -->
+      </div>
       <div class="module">
-        <img src="~/assets/images/man.jpg" alt class="image-background" />
+        <img src="~/assets/images/p_pup.png" alt class="image-background" />
         <h1>P'Pup</h1>
+        <h2>Apaichon Punopas</h2>
       </div>
       <div class="module">
-        <img src="~/assets/images/man.jpg" alt class="image-background" />
+        <img src="~/assets/images/p_tik.png" alt class="image-background" />
         <h1>P'Tick</h1>
+        <h2>Jiraporn Changsamlee</h2>
       </div>
       <div class="module">
-        <img src="~/assets/images/man.jpg" alt class="image-background" />
+        <img src="~/assets/images/p_pp.png" alt class="image-background" />
         <h1>P'PP</h1>
+        <h2>Pornpat Paethong</h2>
       </div>
       <div class="module">
-        <img src="~/assets/images/man.jpg" alt class="image-background" />
+        <img src="~/assets/images/p_ada.png" alt class="image-background" />
         <h1>P'Ada</h1>
+        <h2>Akhane Kaminkure</h2>
       </div>
     </div>
     <div id="sponsors_home">
